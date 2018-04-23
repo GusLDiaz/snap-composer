a34d48f3583c4b9d250d607e5f3fc6188232cb56
Gus Liakos <Gliakos@cnm.edu>
2018-04-23 08:25:28 -0600

    php file and json

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusli
+ * Date: 4/23/2018
+ * Time: 8:09 AM
+ */
+function numLivesPerSenator(string $name): int {
+	if($name === "Arlo") {
+		return 9;
+	} else {
+		return 1;
+	}
+}
+?>
